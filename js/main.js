503ebcc7a668810a5286f63937b5971c0cb2c144
Thanos Samarinas <thanos@paramana.com>
2014-12-15 19:07:40 +0100

    added animation to the nev links

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-   
+
 
     $('.js-menu-icon').click(function(){
-       $('.js-nav').toggleClass('hidden');
+       $('.nav').toggleClass('expanded');
     });
 
 
@@ -71,6 +71,26 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
+
+
 
 });
 
