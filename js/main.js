c73cf9eef0a71af70389637c3c668ecfed07c94c
Giannis <bone.jp@gmail.com>
2015-01-17 17:42:51 +0200

    return for modal window close button

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
 
   $('.pi-close').on( "click", function() {
     $('.modal-window').removeClass('visible').addClass('hidden');
-    $('.container').removeClass('hidden').addClass('visible');
     $('body').removeClass('overflow-hidden');
+    return false;
   });
   $('.project-item--emprego').on( "click", function() {
     $('.modal-window').removeClass('hidden').addClass('visible');
-    $('.container').removeClass('visible').addClass('hidden');
     $('body').addClass('overflow-hidden');
+    return false;
   });
 
 
